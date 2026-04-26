6a16b03a662eba56484959ea965f9bb6b3b82ea9
HrstkaGryze <tomaseposta@protonmail.com>
2026-04-26 14:20:08 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+//------------Hrac--------------
+int HP;
+int maxHP;
+int xp;
+int level;
+int mana;
+int utok;
+int gold;
+
+//-----------Monstra-------------
+int monstraHP;
+int monstrautok;
+int xpDrop;
+int goldDrop;
+
+
+int main() {
+
+cout << "Vítej v Textovém RPG " << endl;
+
+
+
+
+
+cout <<"Diky"
+return 0;
+}
+
