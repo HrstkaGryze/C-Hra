ca3d0b3ab97d82fd3c4e3d28d84d62b74ca7d3c5
HrstkaGryze <tomaseposta@protonmail.com>
2026-05-03 16:50:55 +0200

    Neco

--- main.cpp
+++ main.cpp
@@ -1,32 +1,100 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 //------------Hrac--------------
-int HP;
-int maxHP;
-int xp;
-int level;
-int mana;
-int utok;
-int gold;
 
-//-----------Monstra-------------
-int monstraHP;
-int monstrautok;
-int xpDrop;
-int goldDrop;
-// test zmena pro PR
-//------------Main---------------------
-int main() {
+struct Hrac{
 
-cout << "Vítej v Textovém RPG " << endl;
+    int HP;
+    int MaxHP;
+    int XP;
+    int Level;
+    int Mana;
+    int MaxMana;
+    int Utok;
+    int Gold;
+    string Class;
+};
+
+//-----------Postavy------------
+
+Hrac VyberPostavy(){
+
+    while (true) {
+        cout << "Vyber si postavu" << endl;
+        cout << "1) Rytíř" << endl;
+        cout << "2) Lovec" << endl;
+        cout << "3) Mág" << endl;
+        int C;
+        cin >> C;
+
+        Hrac p;
+
+        if(c==1){
+            p.Class = "Rytíř";
+            p.MaxHP =
+            p.MaxMana = 0;
+            p.Gold = 10;
+            p.Level = 1;
+            p.XP = 0;
+            p.Utok =
+
+        }else if (c==2){
+            p.Class = "Lovec";
+            p.MaxHP =
+            p.MaxMana = 10;
+            p.Gold = 10;
+            p.Level = 1;
+            p.XP = 0;
+            p.Utok =
+
+        }else (c==3){
+            p.Class = "Mág";
+            p.MaxHP =
+            p.MaxMana = 30;
+            p.Gold = 10;
+            p.Level = 1;
+            p.XP = 0;
+            p.Utok =
+        }
 
 
+    }
 
 
-
-cout <<"Diky"
-return 0;
 }
 
+
+
+
+
+//-----------Monstra------------
+struct Monstra{
+
+    int MonstraHP;
+    int MonstraUtok;
+    int XPDrop;
+    int GoldDrop;
+};
+//------------Main--------------
+int main() {
+
+    cout << "Vítej v RPG " << endl;
+
+    cout << "Class: " << Hrac.Class << endl;
+    cout << "Level: " << Hrac.level << "  XP: " << Hrac.xp << endl;
+    cout << "HP: " << Hrac.hp << "/" << Hrac.maxHP
+    cout << "Mana: " << Hrac.mana << "/" << Hrac.maxMana << endl;
+    cout << "Utok: " << Hrac.attack << "  Gold: " << Hrac.gold << endl;
+
+
+
+
+    cout <<"Diky";
+    return 0;
+}
